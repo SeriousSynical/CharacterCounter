61f5d9b26f8ebd745c7276629d2f07c9843f5de8
SeriousSynical <92874565+SeriousSynical@users.noreply.github.com>
2021-11-19 11:11:04 -0500

    Add files via upload

--- CharacterCounter.java
+++ CharacterCounter.java
@@ -2,7 +2,89 @@ import java.util.ArrayList;
 
 public class CharacterCounter {
 
+    // Loose Check
     public static int minRepeat(String string) {
+
+        int min;
+        ArrayList<Integer> repeatList = new ArrayList<>();
+
+        for (char character1 : string.toLowerCase().toCharArray()) {
+
+            int repeats = 0;
+
+            for (char character2 : string.toLowerCase().toCharArray()) {
+
+
+                if (character1 == character2) {
+
+                    repeats++;
+
+                }
+
+
+            }
+
+            repeatList.add(repeats);
+
+        }
+
+        min = repeatList.get(0);
+        for(int i = 1; i < repeatList.size(); i++ ) {
+
+            if(repeatList.get(i) < min) {
+
+                min = repeatList.get(i);
+
+            }
+
+        }
+
+        return min;
+
+    }
+
+    public static int maxRepeat(String string) {
+
+        int max;
+        ArrayList<Integer> repeatList = new ArrayList<>();
+
+        for (char character1 : string.toLowerCase().toCharArray()) {
+
+            int repeats = 0;
+
+            for (char character2 : string.toLowerCase().toCharArray()) {
+
+
+                if (character1 == character2) {
+
+                    repeats++;
+
+                }
+
+
+            }
+
+            repeatList.add(repeats);
+
+        }
+
+        max = repeatList.get(0);
+        for(int i = 1; i < repeatList.size(); i++ ) {
+
+            if(repeatList.get(i) > max) {
+
+                max = repeatList.get(i);
+
+            }
+
+        }
+
+        return max;
+
+    }
+
+    // Strict Check
+    public static int minStrictRepeat(String string) {
 
         int min;
         ArrayList<Integer> repeatList = new ArrayList<>();
@@ -42,7 +124,7 @@ public class CharacterCounter {
 
     }
 
-    public static int maxRepeat(String string) {
+    public static int maxStrictRepeat(String string) {
 
         int max;
         ArrayList<Integer> repeatList = new ArrayList<>();
